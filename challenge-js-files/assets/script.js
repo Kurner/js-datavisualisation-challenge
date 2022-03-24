631008c1ec10f688c85b869b4cf83a47ce55cee6
Loïc Calcagno <calcagnoloic93@gmail.com>
2022-03-24 13:42:35 +0100

    réalisation du graphe

---
+++ challenge-js-files/assets/script.js
@@ -0,0 +1,59 @@
+///// graphique pour les homicides
+let table2 = document.getElementById("table2");
+let tdTable = table2.querySelectorAll("td");
+//console.log(table2);
+
+///// création de sous-tableaux 
+let pays = [];
+let date1 = [];
+let date2 = [];
+
+///// création de la boucle
+for(let x = 0 ; x < tdTable.length ; x++) {
+    if (x % 3  == 0) {
+        pays.push(tdTable[x].innerText)
+    }
+    if (x % 3 == 1) {
+        date1.push(tdTable[x].innerText)
+    }
+    if (x % 3 == 2) {
+        date2.push(tdTable[x].innerText)
+    }
+}
+
+///// création du graphique
+let ctx = document.getElementById("barCanvas")
+let barChart = new Chart(ctx, {
+    type: "bar", 
+    data: {
+        labels: pays,
+        datasets: [{
+            data:date1,
+            label: "2007 à 2009",
+            backgroundColor : "crimson"
+        }, {
+            data:date2, 
+            label: "2010 à 2012", 
+            backgroundColor: "#50C878"
+        }]
+    }, 
+    options: {
+        responsive : true,
+        scales:{
+            y: {
+                ticks :{
+                    font:{
+                        size:12
+                    }
+                }
+            },
+            x: {
+                ticks :{
+                    font:{
+                        size:12
+                    }
+                }
+            }
+        }
+    }
+})
